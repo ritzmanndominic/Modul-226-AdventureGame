e589f6af8f92684f2a558408c3302e1c2f49fea7
xxD04xx <dominic.ritzmann@noseryoung.ch>
2021-10-18 11:47:17 +0200

    door and item class done

--- 20211018-AdventureGame/src/ch/noseryoung/blj/Door.java
+++ 20211018-AdventureGame/src/ch/noseryoung/blj/Door.java
@@ -1,4 +1,30 @@
 package ch.noseryoung.blj;
 
 public class Door {
+    private Room[] connector;
+    private boolean locked;
+
+    public Door() {
+    }
+
+    public Door(Room[] connector, boolean locked) {
+        this.connector = connector;
+        this.locked = locked;
+    }
+
+    public Room[] getConnector() {
+        return connector;
+    }
+
+    public void setConnector(Room[] connector) {
+        this.connector = connector;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
 }
