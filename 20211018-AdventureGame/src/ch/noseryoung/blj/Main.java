8ecc1fe6e072845451f9d73f474efa389dca41a6
Dominic Ritzmann <dominic.ritzmann@noseryoung.ch>
2021-11-06 18:35:20 +0100

    commit

--- 20211018-AdventureGame/src/ch/noseryoung/blj/Main.java
+++ 20211018-AdventureGame/src/ch/noseryoung/blj/Main.java
@@ -7,7 +7,6 @@ public class Main {
         Game game = new Game();
         Player player = new Player();
         Player player1 = new Player(player.getItemList(), 3);
-
         io.switcher(game, player1);
     }
 }
